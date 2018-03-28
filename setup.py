479e77ec40cf6a1c52cc4672302638785ec067cb
editeodoro <enrico.diteodoro@gmail.com>
2018-03-28 11:08:50 +1100

    Add pyBB

--- setup.py
+++ setup.py
@@ -35,49 +35,13 @@ def compileBB():
     print ("OK.")
     
       
-if sys.argv[1]=='sdist':  
-    # If we are creating the sdist package, make a tar with BB source
-    try: remove_tree("pyBBarolo/BBarolo")
-    except: pass
-    try: remove_tree("pyBBarolo/BBarolo.tar.gz")
-    except: pass
-    mkpath("pyBBarolo/BBarolo")    
-    subprocess.call("cp -r src/ pyBBarolo/BBarolo/src", shell=True,stdout=f)
-    subprocess.call("cp -r config/ pyBBarolo/BBarolo/config", shell=True,stdout=f)
-    subprocess.call("cp -r configure pyBBarolo/BBarolo/", shell=True,stdout=f)
-    subprocess.call("cp -r Makefile.in pyBBarolo/BBarolo/", shell=True,stdout=f)
-    subprocess.call("cp -r Makefile.in pyBBarolo/BBarolo/", shell=True,stdout=f)
-    subprocess.call("rm -rf pyBBarolo/BBarolo/src/Build", shell=True,stdout=f)
-    subprocess.call("cd pyBBarolo && tar -czvf BBarolo.tar.gz BBarolo", shell=True,stdout=f)
-    remove_tree("pyBBarolo/BBarolo")
+# First: check if dependencies are available
+modules = ['numpy','astropy']
+for m in modules: checkModule(m)
     
-    # If we creating the dist, additional file is the tar just created
-    package_data = {'pyBBarolo': ['*.tar.gz']}
-    
-else:
-    # First: check if dependencies are available
-    modules = ['numpy','astropy']
-    for m in modules: checkModule(m)
-    
-    # Second: compile BB library
-    if os.path.isdir("./src"):
-        # If we are in BB root, compile from here
-        compileBB()
-        subprocess.call("mv libBB* pyBBarolo/", shell=True, stdout=f)
-    else:
-        # Enter pyBB dir and look for tar file
-        os.chdir("pyBBarolo")
-        if not os.path.isfile("BBarolo.tar.gz"): raise ValueError("BBarolo.tar.gz not found")
-        subprocess.call(["tar xvzf BBarolo.tar.gz"], shell=True, stdout=f)
-        os.chdir("BBarolo")
-        compileBB()
-        os.chdir("../")
-        subprocess.call("mv BBarolo/libBB* .", shell=True, stdout=f)
-        #remove_tree("BBarolo")
-        os.chdir("../")
-    
-    # If installing, the additional data are the compiled libraries
-    package_data = {'pyBBarolo': ['libBB*']}
+
+# If installing, the additional data are the compiled libraries
+package_data = {'pyBBarolo': ['libBB*']}
 
 
 # Installing pyBBarolo package
