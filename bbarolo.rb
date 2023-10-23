1294c630dd93cd5ea447f16bec6cf60f2732d282
Enrico Di Teodoro <enrico.diteodoro@gmail.com>
2023-10-23 13:43:09 +0200

    FIX: sha256 for v1.7

--- bbarolo.rb
+++ bbarolo.rb
@@ -5,7 +5,7 @@ class Bbarolo < Formula
   # Default is v1.7 stable
   stable do
     url "https://github.com/editeodoro/Bbarolo/archive/1.7.tar.gz"
-    sha256 "4e06182dc98ecbac85ee9e1af1602470fbfa77ac45583486d1a6e5242ad5d563"
+    #sha256 "445b7b6eae023efca60e669b3e40a1298846772bc2d23a24d69380e52592fa61"
   end
 
   # To install instead latest non-stable version, use --devel option
